bfc2246f41495190ef606d1e883a91396456cbde
nadfelipe <nadfelipe.s@gmail.com>
2021-07-04 01:41:10 -0300

    up

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const withImages = require('next-images')
+module.exports = withImages()
+// module.exports = {
+//   webpack(config) {
+//     config.module.rules.push({
+//       test: /\.svg$/,
+//       use: ["@svgr/webpack"]
+//     });
+
+//     return config;
+//   }
+// };
--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -3,7 +3,13 @@ import NextLink from 'next/link';
 import styled from 'styled-components';
 
 const LinkStyle = styled.a`
-  background-color: red;
+  background-image: linear-gradient(114.14deg, #740F99 45.88%, #15B4C2 251.47%);
+  padding: 0.7em 1.5em;
+  border-radius: 5px;
+  width: 12rem;
+  text-align: center;
+  color: #f4f4f4;
+  font-weight: 800;
 `;
 
 
